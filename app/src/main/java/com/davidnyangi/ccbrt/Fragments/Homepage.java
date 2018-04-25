62a22ab2081bb7f9307d6b6d43be25e431fcbee0
David Nynagi <davidnyangi@gmail.com>
2018-04-25 20:25:37 +0300

    Adding services layouts and classes

--- app/src/main/java/com/davidnyangi/ccbrt/Fragments/Homepage.java
+++ app/src/main/java/com/davidnyangi/ccbrt/Fragments/Homepage.java
@@ -11,26 +11,53 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.davidnyangi.ccbrt.Adapters.CardAdapter;
 import com.davidnyangi.ccbrt.Adapters.PersonAdapter;
+import com.davidnyangi.ccbrt.Config;
 import com.davidnyangi.ccbrt.Objects.Person;
 import com.davidnyangi.ccbrt.R;
+import com.davidnyangi.ccbrt.Services;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class Homepage extends Fragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
     String param;
     private SliderLayout mDemoSlider;
+    //Creating a List of superheroes
+    private List<Services> listSuperHeroes;
+
+    //Creating Views
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+
+    //Volley Request Queue
+    private RequestQueue requestQueue;
+
+    //The request counter to send ?page=1, ?page=2  requests
+    private int requestCount = 1;
     public Homepage() {
     }
     public static Homepage newInstance(String... params) {
@@ -84,7 +111,27 @@ public class Homepage extends Fragment implements BaseSliderView.OnSliderClickLi
         mDemoSlider.setCustomAnimation(new DescriptionAnimation());
         mDemoSlider.setDuration(4000);
         mDemoSlider.addOnPageChangeListener(this);
+//Initializing Views
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
+        //Initializing our superheroes list
+        listSuperHeroes = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(getActivity());
+
+        //Calling method to get data to fetch data
+        getData();
+
+        //Adding an scroll change listener to recyclerview
+        recyclerView.setOnScrollChangeListener(getActivity());
+
+        //initializing our adapter
+        adapter = new CardAdapter(listSuperHeroes, getActivity());
+
+        //Adding adapter to recyclerview
+        recyclerView.setAdapter(adapter);
         return view;
     }
     @Override
@@ -108,5 +155,91 @@ public class Homepage extends Fragment implements BaseSliderView.OnSliderClickLi
 
     @Override
     public void onPageScrollStateChanged(int state) {}
+    //Request to get json from server we are passing an integer here
+    //This integer will used to specify the page number for the request ?page = requestcount
+    //This method would return a JsonArrayRequest that will be added to the request queue
+    private JsonArrayRequest getDataFromServer(int requestCount) {
+        //Initializing ProgressBar
+        final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar1);
 
+        //Displaying Progressbar
+        progressBar.setVisibility(View.VISIBLE);
+        setProgressBarIndeterminateVisibility(true);
+
+        //JsonArrayRequest of volley
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Config.DATA_URL + String.valueOf(requestCount),
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Calling method parseData to parse the json response
+                        parseData(response);
+                        //Hiding the progressbar
+                        progressBar.setVisibility(View.GONE);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressBar.setVisibility(View.GONE);
+                        //If an error occurs that means end of the list has reached
+                        Toast.makeText(MainActivity.this, "No More Items Available", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        //Returning the request
+        return jsonArrayRequest;
+    }
+
+    //This method will get data from the web api
+    private void getData() {
+        //Adding the method to the queue by calling the method getDataFromServer
+        requestQueue.add(getDataFromServer(requestCount));
+        //Incrementing the request counter
+        requestCount++;
+    }
+
+    //This method will parse json data
+    private void parseData(JSONArray array) {
+        for (int i = 0; i < array.length(); i++) {
+            //Creating the superhero object
+            Services superHero = new Services();
+            JSONObject json = null;
+            try {
+                //Getting json
+                json = array.getJSONObject(i);
+
+                //Adding data to the superhero object
+                superHero.setImageUrl(json.getString(Config.TAG_IMAGE_URL));
+                superHero.setName(json.getString(Config.TAG_NAME));
+                superHero.setPublisher(json.getString(Config.TAG_PUBLISHER));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //Adding the superhero object to the list
+            listSuperHeroes.add(superHero);
+        }
+
+        //Notifying the adapter that data has been added or changed
+        adapter.notifyDataSetChanged();
+    }
+
+    //This method would check that the recyclerview scroll has reached the bottom or not
+    private boolean isLastItemDisplaying(RecyclerView recyclerView) {
+        if (recyclerView.getAdapter().getItemCount() != 0) {
+            int lastVisibleItemPosition = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
+            if (lastVisibleItemPosition != RecyclerView.NO_POSITION && lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() - 1)
+                return true;
+        }
+        return false;
+    }
+
+    //Overriden method to detect scrolling
+    @Override
+    public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+        //Ifscrolled at last then
+        if (isLastItemDisplaying(recyclerView)) {
+            //Calling the method getdata again
+            getData();
+        }
+    }
 }
