3de689f6038713745c24b45c9b08c830e545dc1a
David Nynagi <davidnyangi@gmail.com>
2018-04-25 19:52:44 +0300

    Adding Config to get services from db

---
+++ app/src/main/java/com/davidnyangi/ccbrt/Services.java
@@ -0,0 +1,38 @@
+package com.davidnyangi.ccbrt;
+
+/**
+ * Created by nyangi.admin on 4/25/2018.
+ */
+
+public class Services {
+
+    //Data Variables
+    private String imageUrl;
+    private String name;
+    private String publisher;
+
+    //Getters and Setters
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
