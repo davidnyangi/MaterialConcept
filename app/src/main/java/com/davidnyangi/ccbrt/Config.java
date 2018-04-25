04caf714869b9f582d66ff1710a76352ef259e2c
David Nynagi <davidnyangi@gmail.com>
2018-04-25 19:38:23 +0300

    Adding Config to get services from db

---
+++ app/src/main/java/com/davidnyangi/ccbrt/Config.java
@@ -0,0 +1,15 @@
+package com.davidnyangi.ccbrt;
+
+/**
+ * Created by nyangi.admin on 4/25/2018.
+ */
+
+public class Config {
+    //Data URL
+    public static final String DATA_URL = "https://davidnyangiprojects.com/Projects/Android/CCBRT/getAllServices.php?page=";
+
+    //JSON TAGS
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_PUBLISHER = "desc";
+}
